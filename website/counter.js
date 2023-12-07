cf6bee54ee1409080347a32c2e2163475cf7bead
Gadagoju Shiva <133100130+GadagojuShiva@users.noreply.github.com>
2023-12-07 19:00:08 +0530

    Update counter.js

--- website/counter.js
+++ website/counter.js
@@ -1,22 +1,25 @@
-// const counter = document.querySelector(".counter-number");
-// async function updateCounter() {
-// 	let response = await fetch("https://qrfdaro455gqfagi4b25ubgxmy0lezan.lambda-url.us-east-1.on.aws/");
-// 	let data = await response.json();
-// 	counter.innerHTML = `Views: $[data]`;
-// }
-// updateCounter();
-
+// Select the HTML element with the class "counter-number" and store it in the variable 'counter'
 const counter = document.querySelector(".counter-number");
 
+// Define an asynchronous function named 'updateCounter'
 async function updateCounter() {
-	try {
-		let response = await fetch("https://qrfdaro455gqfagi4b25ubgxmy0lezan.lambda-url.us-east-1.on.aws/"); // Replace with the correct endpoint
-		let data = await response.json();
-		counter.innerHTML = `Views: ${data}`;
-	} catch (error) {
-		console.error("Error fetching data:", error);
-		counter.innerHTML = "Couldn't retrieve views";
-	}
+    try {
+        // Make an asynchronous fetch request to the specified endpoint
+        let response = await fetch("https://qrfdaro455gqfagi4b25ubgxmy0lezan.lambda-url.us-east-1.on.aws/"); // Replace with the correct endpoint
+
+        // Parse the response body as JSON and store it in the variable 'data'
+        let data = await response.json();
+
+        // Update the content of the 'counter' element with the retrieved data
+        counter.innerHTML = `Views: ${data}`;
+    } catch (error) {
+        // If an error occurs during the fetch or JSON parsing, log the error to the console
+        console.error("Error fetching data:", error);
+
+        // Update the content of the 'counter' element with an error message
+        counter.innerHTML = "Couldn't retrieve views";
+    }
 }
 
+// Call the 'updateCounter' function to initiate the data fetching and updating process
 updateCounter();
